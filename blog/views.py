20e15ff8d38d0740127e71348b659d520f7ecafb
SahilRaj1 <raj24sahil@gmail.com>
2022-09-05 18:02:51 +0530

    commit

--- blog/views.py
+++ blog/views.py
@@ -1,29 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
-posts = [
-    {
-        'author': 'Sahil Raj',
-        'title': 'Jod Blog',
-        'content': 'My name is Sahil Raj',
-        'date_posted': 'September 27, 2021'
-    },
-    {
-        'author': 'Ritika Jagtap',
-        'title': 'Noob Blog',
-        'content': 'My name is Ritika Jagtap',
-        'date_posted': 'October 17, 2021'
-    }
-]
+from .models import Post
 
 # Create your views here.
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
 def about(request):
     return render(request, 'blog/about.html', {'title': 'About'})
-
